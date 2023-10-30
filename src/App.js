b8c000f961aa2bf07c64d93586188abdb82458ae
chocopie777 <egoran99@gmail.com>
2023-10-30 14:11:30 +0500

    base concepts

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import styled from "styled-components";
 import Title from "./components/Title";
 import Flex from "./components/Flex";
+import Console from "./components/Console";
+import Button from "./components/Button";
 
 const AppWrapper = styled.div`
   width: 100%;
@@ -18,6 +20,10 @@ function App() {
                 <Title color={'green'}>
                     app
                 </Title>
+            </Flex>
+            <Flex direction='column'>
+                <Console/>
+                <Button outlined color={'green'} align={'flex-end'}>Отправить</Button>
             </Flex>
         </AppWrapper>
     );
--- src/components/Console.js
+++ src/components/Console.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import styled from "styled-components";
 
-const Console = () => {
+const StyledConsole = styled.textarea`
+  width: 100%;
+  height: 70vh;
+  background: black;
+  font-size: 24px;
+  border: none;
+  color: ${({color}) => color || 'white'};
+  resize: none;
+  &:focus {
+    outline: none;
+  }
+`
+
+const Console = (props) => {
     return (
-        <div>
-            
-        </div>
+        <StyledConsole {...props}/>
     );
 };
 
